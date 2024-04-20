17b5f0c20017a0a9e1b2bca39c985dd99cc3a455
tiger <thisisstreamliner@gmail.com>
2024-04-20 17:47:23 +0300

    feat: create app layout

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Our react native blank canvas</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  search: {
+    backgroundColor: '#11EE11',
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: '#1e1eAA',
+    alignSelf: 'stretch',
+    padding: 16
+  }
 });
