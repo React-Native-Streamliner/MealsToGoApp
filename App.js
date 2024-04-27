e3862f4fc21dbcadcd62b9bced6c030968f665d5
tiger <thisisstreamliner@gmail.com>
2024-04-27 15:33:43 +0300

    feat: load custom fonts

--- App.js
+++ App.js
@@ -2,7 +2,22 @@ import { ThemeProvider } from "styled-components";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurant.screen";
 import { theme } from "./src/infastructure/theme";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Oswald_400Regular,
+  });
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
