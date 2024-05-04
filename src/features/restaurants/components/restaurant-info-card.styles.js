c67b0c18c38d645280e2677cc4184c8726d21394
tiger <thisisstreamliner@gmail.com>
2024-05-04 11:01:22 +0300

    feat: complete flatlist

--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -1,39 +1,42 @@
 import styled from "styled-components";
+import { Card } from "react-native-paper";
+import { Text, View, Image } from "react-native";
 
-export const RestaurantCard = styled.Card`
+export const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
+  margin-bottom: ${(props) => props.theme.space[3]};
 `;
 
-export const RestaurantCardCover = styled.Card.Cover`
+export const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-export const Address = styled.Text`
+export const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
-export const Info = styled.View`
+export const Info = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-export const ClosedView = styled.View`
+export const ClosedView = styled(View)`
   flex-direction: row;
 `;
 
-export const Details = styled.View`
+export const Details = styled(View)`
   flex-direction: row;
   justify-content: space-between;
 `;
 
-export const Rating = styled.View`
+export const Rating = styled(View)`
   flex-direction: row;
   padding-top: ${(props) => props.theme.space[2]};
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
-export const Icon = styled.Image`
+export const Icon = styled(Image)`
   width: 15px;
   height: 15px;
 `;
--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -20,7 +20,14 @@ const RestaurantsScreen = () => {
         <Searchbar />
       </SearchContainer>
       <FlatList
-        data={[{ name: 1 }, { name: 2 }]}
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+        ]}
         renderItem={() => <RestaurantInfoCard />}
         keyExtractor={(item) => item.name}
         contentContainerStyle={{ padding: 16 }}
