9d554efb464599bb17a2c988f3ef717db6337f84
tiger <thisisstreamliner@gmail.com>
2024-05-06 12:45:40 +0300

    feat: implement tab icons

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { theme } from "./src/infastructure/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import {
   useFonts as useOswald,
@@ -43,7 +44,24 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              // eslint-disable-next-line react/no-unstable-nested-components
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === "Restaurants") {
+                  iconName = "restaurant";
+                } else if (route.name === "Map") {
+                  iconName = "map";
+                } else if (route.name === "Settings") {
+                  iconName = "settings";
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "grey",
+            })}
+          >
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={MapScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
